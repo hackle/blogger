951431bf3bf5f0f48e7ea4cee3eca6c66e8d0465
root <root@DESKTOP-BTT3QCJ.localdomain>
2018-04-03 21:16:17 +1200

    works

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import qualified Data.Aeson as Aeson
+import           AWSLambda.Events.APIGateway
+import           Control.Lens
+import qualified Data.HashMap.Strict         as HashMap
+import           Data.Semigroup
+import           Data.Text                   (Text)
 
-import AWSLambda (lambdaMain)
+main :: IO ()
+main = apiGatewayMain hello
 
-main = lambdaMain handler
-
-handler :: Aeson.Value -> IO [Int]
-handler evt = do
-    putStrLn "This should go to logs"
-    print evt
-    pure [1, 2, 3]
+hello :: APIGatewayProxyRequest Text -> IO (APIGatewayProxyResponse Text)
+hello request = do
+  putStrLn "This should go to logs"
+  case HashMap.lookup "name" (request ^. agprqPathParameters) of
+    Just name -> return $ responseOK & responseBody ?~ "Hello, " <> name
+    Nothing -> return responseNotFound
