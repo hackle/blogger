08c617dd483207565653ad0ba0ee4a34cb8e7177
Hackle Wayne <hacklew@hotmail.com>
2018-04-25 21:19:40 +1200

    still not there for root relative path

--- app/Main.hs
+++ app/Main.hs
@@ -5,23 +5,31 @@ import           AWSLambda.Events.APIGateway
 import           Control.Lens
 import qualified Data.HashMap.Strict         as HashMap
 import           Data.Semigroup
-import           Data.Text                   (Text, unpack, toLower)
+import           Data.Text                   (Text, unpack, toLower, pack)
 import Blog
 import Text.Blaze.Html
+import Control.Monad.Trans.Maybe
+import Data.Maybe
+import Control.Applicative
 
 main :: IO ()
 main = apiGatewayMain pageIndex
 
-success :: Html -> APIGatewayProxyResponse Text
-success body = responseOK 
-    & responseBody ?~ renderPage body
+success :: BasePath -> Html -> APIGatewayProxyResponse Text
+success base body = responseOK 
+    & responseBody ?~ renderPage body base
     & agprsHeaders `over` HashMap.insert "content-type" "text/html"
-    
+
+tryLoad :: BasePath -> FilePath -> IO (APIGatewayProxyResponse Text)
+tryLoad base path = do
+    f1 <- runMaybeT $ loadArticle path
+    if isJust f1 
+        then return $ success "" (fromJust f1)
+        else success base <$> (mkArticlePath "index" >>= readArticle)
+
 pageIndex :: APIGatewayProxyRequest Text -> IO (APIGatewayProxyResponse Text)
-pageIndex request =
-  let path = HashMap.lookup "name" (request ^. agprqPathParameters)
-      name = maybe "index" toLower path
-      in do
-        articlePath <- getArticlePath "index" (unpack name)
-        article <- readArticle articlePath
-        return $ success article
+pageIndex request = do
+    let urlPath = request ^. agprqPath
+    putStrLn (unpack urlPath)
+    let path = HashMap.lookup "name" (request ^. agprqPathParameters)
+    tryLoad urlPath (maybe "" unpack path)
--- src/Blog.hs
+++ src/Blog.hs
@@ -12,11 +12,15 @@ import Control.Monad (forM_, guard)
 import qualified Data.Text.Lazy as LT
 import System.Directory
 import Data.Text
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans
 
 type HtmlBody = H.Html
 type ArticleName = String
 type DefaultArticleName = ArticleName
 type ValidFilePath = FilePath
+type BasePath = Text
+type CurrentDirectory = FilePath
 
 cannotFindDefaultArticle :: String
 cannotFindDefaultArticle = "Cannot find default article"
@@ -26,24 +30,30 @@ readArticle fn = do
     content <- IOT.readFile fn
     return $ markdown def content
 
-makePage :: HtmlBody -> H.Html
-makePage body = H.docTypeHtml $ do
-  H.head $
+makePage :: HtmlBody -> BasePath -> H.Html
+makePage body base = H.docTypeHtml $ do
+  H.head $ do
     H.title "hackmann's blog"
+    H.base ! href (H.toValue base)
   H.body $ do
     H.h1 $
       H.a ! href "index" $ "hackmann"
     H.span "experienced imperative programmer turned functional advocate"
     body
 
-renderPage :: HtmlBody -> Text
-renderPage body = LT.toStrict $ renderHtml (makePage body)
+renderPage :: HtmlBody -> BasePath -> Text
+renderPage body base = LT.toStrict $ renderHtml (makePage body base)
 
-getArticlePath :: DefaultArticleName -> ArticleName -> IO FilePath
-getArticlePath def an = do
+mkArticlePath :: ArticleName -> IO FilePath
+mkArticlePath an = do
   cd <- getCurrentDirectory
-  let fp = cd ++ "/src/raw/" ++ an ++ ".md"
-  valid <- doesFileExist fp
+  return $ cd ++ "/src/raw/" ++ an ++ ".md"
+
+loadArticle :: ArticleName -> MaybeT IO Html
+loadArticle an = do 
+  fp <- lift $ mkArticlePath an
+  valid <- lift $ doesFileExist fp
   if valid 
-    then return fp 
-    else getArticlePath (error cannotFindDefaultArticle) def
+    then lift $ readArticle fp
+    else MaybeT $ return Nothing
+    
